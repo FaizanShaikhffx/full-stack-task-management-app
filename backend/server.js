49cf81f992358762874fbf5b18471d988c0584e3
Faizan Shaikh <iamfaizzan@gmail.com>
2025-01-16 13:43:10 +0530

    Validation & Error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express = require('express'); 
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser'); 
-
+const authRoutes = require('./routes/routes.auth.js');
+const menuRoutes = require('./routes/routes.menu.js');
+const orderRoutes = require('./routes/routes.order.js')
 
 const app = express(); 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use('/auth', authRoutes); 
+app.use('/menu', menuRoutes); 
+app.use('/order', orderRoutes); 
 
-mongoose.connect('mongodb+srv://faizanshaikh3122002:faizan123@cluster0.i7gog.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true, 
 });
